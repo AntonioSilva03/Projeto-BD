e7fab8f021eb4f35988aff5becd1eb23695c2516
Pedrosilva03 <pedroo.silva180@gmail.com>
2023-06-05 22:21:37 +0100

    Atributos não nulos

--- basededados.sql
+++ basededados.sql
@@ -14,87 +14,87 @@ DROP TABLE IF EXISTS SeguroSaude;
 DROP TABLE IF EXISTS Inventario;
 
 CREATE TABLE IF NOT EXISTS SeguroSaude(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    seguradora VARCHAR(100),
-    validade date,
-    percentgem FLOAT
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    seguradora VARCHAR(100) NOT NULL,
+    validade date NOT NULL,
+    percentgem FLOAT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Paciente(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    nascimento date,
-    idade INTEGER,
-    genero char,
-    morada VARCHAR(100),
-    telefone INTEGER,
-    email VARCHAR(100),
-    emergencia INTEGER,
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    nascimento date NOT NULL,
+    idade INTEGER NOT NULL,
+    genero char NOT NULL,
+    morada VARCHAR(100) NOT NULL,
+    telefone INTEGER NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    emergencia INTEGER NOT NULL,
     id_seguro INT,
     FOREIGN KEY (id_seguro) REFERENCES SeguroSaude(identificador)
 );
 
 CREATE TABLE IF NOT EXISTS Especialidade(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100)
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Medico(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    idade INTEGER,
-    genero char,
-    morada VARCHAR(100),
-    telefone INTEGER,
-    email VARCHAR(100),
-    idEspecialidade INTEGER,
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    idade INTEGER NOT NULL,
+    genero char NOT NULL,
+    morada VARCHAR(100) NOT NULL,
+    telefone INTEGER NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    idEspecialidade INTEGER NOT NULL,
     FOREIGN KEY (idEspecialidade) REFERENCES Especialidade(identificador) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Consulta(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    data_consulta DATE,
-    idMedico INTEGER,
-    idPaciente INTEGER,
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    data_consulta DATE NOT NULL,
+    idMedico INTEGER NOT NULL,
+    idPaciente INTEGER NOT NULL,
     FOREIGN KEY (idMedico) REFERENCES Medico(identificador) ON DELETE CASCADE,
     FOREIGN KEY (idPaciente) REFERENCES Paciente(identificador) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Medicamentos(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    idConsulta INT,
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    idConsulta INT NOT NULL,
     FOREIGN KEY (idConsulta) REFERENCES Consulta(identificador) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Inventario(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100)
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Exames(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    descricao VARCHAR(1000),
-    idPaciente INTEGER,
-    idInventario INT,
-    idConsulta INT,
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(1000) NOT NULL,
+    idPaciente INTEGER NOT NULL,
+    idInventario INT NOT NULL,
+    idConsulta INT NOT NULL,
     FOREIGN KEY (idInventario) REFERENCES Inventario(identificador) ON DELETE CASCADE,
     FOREIGN KEY (idConsulta) REFERENCES Consulta(identificador) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS HistorialMedico(
-	identificador INTEGER PRIMARY KEY AUTO_INCREMENT,
-	idPaciente INTEGER,
+	identificador INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	idPaciente INTEGER NOT NULL,
     medicamentos VARCHAR(100),
     diagnosticos VARCHAR(100),
     FOREIGN KEY (idPaciente) REFERENCES Paciente(identificador) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS ConsultaHistorial(
-	idConsulta INT,
+	idConsulta INT NOT NULL,
     FOREIGN KEY (idConsulta) REFERENCES Consulta(identificador) ON DELETE CASCADE,
-    idHistorial INT,
+    idHistorial INT NOT NULL,
     FOREIGN KEY (idHistorial) REFERENCES HistorialMedico(identificador) ON DELETE CASCADE,
-    atualizacao VARCHAR(100)
+    atualizacao VARCHAR(100) NOT NULL
 );
